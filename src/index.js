bfe58cd9e0e62792531d337dd87fd45fccbb4fcd
Rohit Rai <66778858+GabbarVirus@users.noreply.github.com>
2023-04-03 12:06:44 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import "./styles/App.css"
 
 ReactDOM.render(
-  <div>
-    
-      <App />
-    
-  </div>,
-  document.getElementById("root")
+  <App />,
+  document.getElementById('root')
 );
